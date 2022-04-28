69f6dea09f8b3cf20b444f0f9fbbfc7901ffec6e
Bruno Flores <bruno@brunoflores.com.br>
2022-04-29 06:55:33 +1000

    Format all files.

--- lib/syntax.mli
+++ lib/syntax.mli
@@ -4,7 +4,7 @@ open Support.Error
 
 (* Data type definitions *)
 type ty =
-    TyVar of int * int
+  | TyVar of int * int
   | TyString
   | TySome of string * ty
   | TyArr of ty * ty
@@ -17,7 +17,7 @@ type ty =
   | TyFloat
 
 type term =
-    TmAscribe of info * term * ty
+  | TmAscribe of info * term * ty
   | TmString of info * string
   | TmPack of info * ty * term * ty
   | TmUnpack of info * string * string * term * term
@@ -43,24 +43,25 @@ type term =
   | TmFix of info * term
 
 type binding =
-    NameBind 
+  | NameBind
   | TyVarBind
   | VarBind of ty
   | TyAbbBind of ty
-  | TmAbbBind of term * (ty option)
+  | TmAbbBind of term * ty option
 
 type command =
-    Import of string
+  | Import of string
   | Eval of info * term
   | Bind of info * string * binding
   | SomeBind of info * string * string * term
 
 (* Contexts *)
 type context
-val emptycontext : context 
+
+val emptycontext : context
 val ctxlength : context -> int
 val addbinding : context -> string -> binding -> context
-val addname: context -> string -> context
+val addname : context -> string -> context
 val index2name : info -> context -> int -> string
 val getbinding : info -> context -> int -> binding
 val name2index : info -> context -> string -> int
@@ -68,18 +69,17 @@ val isnamebound : context -> string -> bool
 val getTypeFromContext : info -> context -> int -> ty
 
 (* Shifting and substitution *)
-val termShift: int -> term -> term
-val termSubstTop: term -> term -> term
+val termShift : int -> term -> term
+val termSubstTop : term -> term -> term
 val typeShift : int -> ty -> ty
-val typeSubstTop: ty -> ty -> ty
-val tytermSubstTop: ty -> term -> term
+val typeSubstTop : ty -> ty -> ty
+val tytermSubstTop : ty -> term -> term
 
 (* Printing *)
-val printtm: context -> term -> unit
-val printtm_ATerm: bool -> context -> term -> unit
+val printtm : context -> term -> unit
+val printtm_ATerm : bool -> context -> term -> unit
 val printty : context -> ty -> unit
 val prbinding : context -> binding -> unit
 
 (* Misc *)
-val tmInfo: term -> info
-
+val tmInfo : term -> info
